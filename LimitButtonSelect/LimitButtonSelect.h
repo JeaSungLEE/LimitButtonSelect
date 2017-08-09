6308dd240e835bdd488822dc7f3173a9815a54e2
jeasung.lee <jeasung.lee@navercorp.com>
2017-08-09 11:08:27 +0900

    Initial Commit

---
+++ LimitButtonSelect/LimitButtonSelect.h
@@ -0,0 +1,19 @@
+//
+//  LimitButtonSelect.h
+//  LimitButtonSelect
+//
+//  Created by jeasung.lee on 2017. 8. 9..
+//  Copyright © 2017년 jeasung.lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LimitButtonSelect.
+FOUNDATION_EXPORT double LimitButtonSelectVersionNumber;
+
+//! Project version string for LimitButtonSelect.
+FOUNDATION_EXPORT const unsigned char LimitButtonSelectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LimitButtonSelect/PublicHeader.h>
+
+
